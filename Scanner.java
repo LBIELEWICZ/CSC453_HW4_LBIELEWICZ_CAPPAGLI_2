f111171c80befa1b37975e5c70bb57483a030a6c
FyraFlare <cappagli@email.arizona.edu>
2019-10-28 22:12:50 -0700

    Added new token types and handling to Scanner

--- Scanner.java
+++ Scanner.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 
 class Token{
   enum TokenType{
-    NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, OP, CP, ID, ASSG, EQ, NEQ, INT, END, OB, CB, IF, WHILE, VOID;
+    NUM, PLUS, MINUS, MUL, DIV, LT, LTE, GT, GTE, OP, CP, ID, ASSG, EQ, NEQ, INT, END, OB, CB, IF, WHILE, VOID, PUBLIC, PRIVATE, CLASS, AND, OR;
   }
 
   TokenType tokenType;
@@ -71,6 +71,24 @@ public class Scanner{
       ret = new Token(Token.TokenType.VOID, "void");
       itr = itr + 4;
     }
+
+    // Handling a PUBLIC token
+    else if (stream.length() > 5 && stream.substring(itr,itr+6).equals("public")) {
+      ret = new Token(Token.TokenType.PUBLIC, "public");
+      itr = itr + 6;
+    }
+
+    // Handling a PRIVATE token
+    else if (stream.length() > 6 && stream.substring(itr,itr+7).equals("private")) {
+      ret = new Token(Token.TokenType.PRIVATE, "private");
+      itr = itr + 7;
+    }
+
+    // Handling a CLASS token
+    else if (stream.length() > 4 && stream.substring(itr,itr+5).equals("class")) {
+      ret = new Token(Token.TokenType.CLASS, "class");
+      itr = itr + 5;
+    }
     
     // Handling an ID token
     else if ((tokChar >= 'a' && tokChar <= 'z') || (tokChar >= 'A' && tokChar <= 'Z')) {
@@ -112,6 +130,18 @@ public class Scanner{
     else if (tokChar == '/') {
       ret = new Token(Token.TokenType.DIV, "/");
       itr++;
+    }
+
+    // Handling an AND token
+    else if (stream.length() > 1 && tokChar == '&' && stream.charAt(itr + 1) == '&') {
+      ret = new Token(Token.TokenType.AND, "&&");
+      itr = itr + 2;
+    }
+
+    // Handling an OR token
+    else if (stream.length() > 1 && tokChar == '|' && stream.charAt(itr + 1) == '|') {
+      ret = new Token(Token.TokenType.OR, "||");
+      itr = itr + 2;
     }
     
     // Handling an NEQ token
